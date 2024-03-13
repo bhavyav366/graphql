72f2d2c1f6b0c2cb228a677d44c3e876f66f62e4
bhavya <bhavyaverma910@gmail.com>
2024-03-13 20:42:19 +0530

    first commit

---
+++ client/src/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  HttpLink,
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: "http://localhost:4000/graphql",
+  }),
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
+
+
+reportWebVitals();
---
+++ server/index.js
@@ -0,0 +1,52 @@
+const express = require('express')
+const { ApolloServer } = require('apollo-server-express')
+const mongoose = require('mongoose')
+const { gql } = require('apollo-server-express')
+const typeDefs = require('./typeDefs')
+const resolvers = require('./resolvers')
+
+
+
+
+const URL = "mongodb+srv://bhavya:bhavya12345@atlascluster.t5lfxfj.mongodb.net/";
+
+mongoose.connect(URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log('DB Connected');
+        // Your code here
+    })
+    .catch(err => {
+        console.error('Error connecting to MongoDB:', err);
+    });
+
+
+/*const URL = "mongodb+srv://bhavya:<bhavya12345>@atlascluster.t5lfxfj.mongodb.net/"
+
+mongoose.connect(URL, {
+
+    })
+    .then(() => {
+        console.log('DB Connected');
+        // Your code here
+    })
+    .catch(err => {
+        console.error('Error connecting to MongoDB:', err);
+    });*/
+
+
+
+const startServer = async() => {
+    const app = express();
+    const apolloServer = new ApolloServer({
+        typeDefs,
+        resolvers,
+    });
+    await apolloServer.start();
+    apolloServer.applyMiddleware({ app }); // Change apolloServerServer to apolloServer
+    app.listen(4000, () => console.log("Server is running at http://localhost:4000"));
+};
+
+startServer();
